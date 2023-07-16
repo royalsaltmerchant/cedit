8652f7b738599447ca5a7a12a69df556fd50317a
Julian <jumpingafterrain@gmail.com>
2023-07-17 01:05:23 +0200

    update styling and add open/save

--- main.cpp
+++ main.cpp
@@ -1,32 +1,33 @@
 #include <wx/wx.h>
 #include <wx/stc/stc.h>
+// test
 
 wxColour white(220, 220, 220);
 wxColour grey(49, 56, 72);
-wxColour light_grey(87, 96, 114);
+wxColour light_grey(107, 116, 134);
 wxColour dark_grey(36, 42, 54);
 
-wxColour red(255, 145, 145);
-wxColour blue(50, 150, 255);
-wxColour light_blue(0, 225, 255);
-wxColour green(0, 255, 0);
-wxColour orange(255, 165, 50);
+wxColour red(225, 165, 165);
+wxColour purple(180, 160, 200);
+wxColour light_blue(155, 205, 225);
+wxColour green(160, 200, 160);
+wxColour orange(235, 175, 125);
 
 
 
 class MyTextEditor : public wxFrame {
 public:
-    MyTextEditor() : wxFrame(NULL, wxID_ANY, "Text Editor", wxDefaultPosition, wxSize(600, 400)) {
+    MyTextEditor(const wxString& filename) : wxFrame(NULL, wxID_ANY, "Text Editor", wxDefaultPosition, wxSize(600, 400)) {
         textCtrl = new wxStyledTextCtrl(this, wxID_ANY);
 
         // Set styles
-        wxFont font(14, wxFONTFAMILY_TELETYPE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+        wxFont font(14, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
 
         textCtrl->StyleSetForeground(wxSTC_STYLE_DEFAULT, white); // Set default text color to white
         textCtrl->StyleSetBackground(wxSTC_STYLE_DEFAULT, dark_grey); // Set default background color to black
 
 
-        textCtrl->StyleSetFont(wxFONTFAMILY_TELETYPE, font);
+        textCtrl->StyleSetFont(wxFONTFAMILY_DEFAULT, font);
         // textCtrl->SetWrapMode(wxSTC_WRAP_WORD);
 
                 // Setting block-sized caret
@@ -49,9 +50,10 @@ public:
         textCtrl->StyleSetForeground(wxSTC_C_COMMENTDOC, light_grey);
         textCtrl->StyleSetForeground(wxSTC_C_NUMBER, red);
         textCtrl->StyleSetForeground(wxSTC_C_WORD, light_blue);
-        textCtrl->StyleSetForeground(wxSTC_C_STRING, orange);
-        textCtrl->StyleSetForeground(wxSTC_C_CHARACTER, orange);
-        textCtrl->StyleSetForeground(wxSTC_C_OPERATOR, blue);
+        textCtrl->StyleSetForeground(wxSTC_C_STRING, green);
+        textCtrl->StyleSetForeground(wxSTC_C_CHARACTER, green);
+        textCtrl->StyleSetForeground(wxSTC_C_OPERATOR, purple);
+        textCtrl->StyleSetForeground(wxSTC_C_PREPROCESSOR, orange);
 
         textCtrl->StyleSetFont(wxSTC_C_COMMENT, font);
         textCtrl->StyleSetFont(wxSTC_C_COMMENTLINE, font);
@@ -62,34 +64,60 @@ public:
         textCtrl->StyleSetFont(wxSTC_C_CHARACTER, font);
         textCtrl->StyleSetFont(wxSTC_C_OPERATOR, font);
 
-        wxString keywords = "class int float while private char catch import module export virtual operator sizeof "
-                            "dynamic_cast typedef const_cast const for static_cast union namespace unsigned long "
-                            "volatile static protected bool template mutable if public friend do return goto auto "
-                            "void enum else break extern using asm case typeid short reinterpret_cast default "
-                            "double register explicit signed typename try this switch continue wchar_t inline "
-                            "delete alignas alignof char16_t char32_t constexpr decltype noexcept nullptr "
-                            "static_assert thread_local restrict _Bool complex _Complex _Imaginary atomic_bool "
-                            "atomic_char atomic_schar atomic_uchar atomic_short atomic_ushort atomic_int atomic_uint "
-                            "atomic_long atomic_ulong atomic_llong atomic_ullong new throw";
+        wxString keywords = "alignas alignof and and_eq asm auto bitand bitor bool break case catch char char8_t char16_t char32_t class compl concept const consteval constexpr const_cast continue co_await co_return co_yield decltype default delete do double dynamic_cast else enum explicit export extern false float for friend goto if inline int long mutable namespace new noexcept not not_eq nullptr operator or or_eq private protected public register reinterpret_cast requires return short signed sizeof static static_assert static_cast struct switch synchronized template this thread_local throw true try typedef typeid typename union unsigned using virtual void volatile wchar_t while xor xor_eq";
         textCtrl->SetKeyWords(0, keywords);
 
         // Events
         textCtrl->Bind(wxEVT_STC_CHANGE, &MyTextEditor::OnTextChanged, this);
+
+        // load
+        // Open the file if a filename was provided
+        if (!filename.IsEmpty()) {
+            textCtrl->LoadFile(filename);
+            currentFile = filename;
+        }
+
+        // Create a menu bar
+        wxMenuBar* menuBar = new wxMenuBar;
+        wxMenu* fileMenu = new wxMenu;
+        fileMenu->Append(wxID_SAVE, "&Save\tCtrl-S", "Save the current document");
+        menuBar->Append(fileMenu, "&File");
+        SetMenuBar(menuBar);
+
+        // Bind the save event
+        Bind(wxEVT_MENU, &MyTextEditor::OnSave, this, wxID_SAVE);
+
     }
 
 private:
     wxStyledTextCtrl* textCtrl;
+    wxString currentFile;
 
     void OnTextChanged(wxStyledTextEvent& event) {
         textCtrl->SetMarginWidth(0, textCtrl->TextWidth(wxSTC_STYLE_LINENUMBER, "_99999"));
         event.Skip();
+    }
+    
+    void OnSave(wxCommandEvent& event) {
+        if (currentFile.IsEmpty()) {
+            wxFileDialog saveFileDialog(this, "Save as", "", "", "Text files (*.txt)|*.txt|All files (*.*)|*.*", wxFD_SAVE|wxFD_OVERWRITE_PROMPT);
+            if (saveFileDialog.ShowModal() == wxID_CANCEL)
+                return; // The user cancelled the operation
+            currentFile = saveFileDialog.GetPath();
+        }
+        textCtrl->SaveFile(currentFile);
     }
 };
 
 class MyApp : public wxApp {
 public:
     virtual bool OnInit() {
-        MyTextEditor* editor = new MyTextEditor();
+        wxString filename;
+        if (argc > 1) {
+            filename = argv[1];
+        }
+
+        MyTextEditor* editor = new MyTextEditor(filename);
         editor->Show();
         return true;
     }
